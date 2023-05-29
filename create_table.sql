a7d978781bb4d9c418e7db4ea3c8fb6d06e0dd91
ZhouHaiwei <113036309+zh0uhaiwei@users.noreply.github.com>
2023-05-29 08:43:26 +0800

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -1,8 +1,7 @@
 CREATE TABLE `prom_http_sd_web` (
-  `prom_ip` varchar(128) COLLATE utf8_bin NOT NULL,
-  `job` varchar(64) COLLATE utf8_bin NOT NULL,
-  `app` varchar(128) COLLATE utf8_bin NOT NULL,
-  `gp` varchar(64) COLLATE utf8_bin DEFAULT NULL COMMENT 'group',
-  `target_ip` varchar(128) COLLATE utf8_bin NOT NULL,
-  `target_port` int(11) NOT NULL
+  `prom` varchar(18) COLLATE utf8_bin NOT NULL,
+  `job` varchar(20) COLLATE utf8_bin NOT NULL,
+  `labels` varchar(128) COLLATE utf8_bin NOT NULL,
+  `target_ip` varchar(18) COLLATE utf8_bin NOT NULL,
+  `target_port` int(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
